a26b1b1c6941712efc9ca24e4c2da371429d69e4
Danial Motahari <danial.motahari@rokt.com>
2021-04-26 14:55:27 +1000

    update target

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     products: [
         .library(
             name: "Rokt_Widget",
-            targets: ["Rokt-Widget"]),
+            targets: ["Rokt_Widget"]),
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "Rokt-Widget",
+        .binaryTarget(name: "Rokt_Widget",
                       url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.5.0-alpha.2487/Rokt_Widget.xcframework.zip",
                       checksum: "5c3ba025b886c4a8c024796b72caa4bea0b3e68548cd8fddafa9ba3fe7aee4de")
     ]
