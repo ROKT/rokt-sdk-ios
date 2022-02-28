9b8cd936d6bd561f9dc9a119ee040abcc7eee526
danial motahari <danial.motahari@rokt.com>
2022-02-28 15:19:27 +1100

    update to version 3.6.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Rokt_Widget",
-                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.6.1/Rokt_Widget.xcframework.zip",
-                      checksum: "90db1ab77c0095c30e824158932c6b9a5ebe6ccffad45887638d4031004fdba8")
+                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.6.2/Rokt_Widget.xcframework.zip",
+                      checksum: "c0f8908ff44a2c6c8abd097e111f6c4d0df962287886ffb9bed40df86a01a0ac")
     ]
 )
