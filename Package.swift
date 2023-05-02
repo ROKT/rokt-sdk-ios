880a6e9e56ea4f44bef52fb2478223fc570fade1
mirannakamura <122853726+mirannakamura@users.noreply.github.com>
2023-05-02 15:58:24 +1000

    update for 3.13.0 (#21)
    * update package.swift for 3.13.0
    
    * update readme
    
    * update readme

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Rokt_Widget",
-                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.12.1/Rokt_Widget.xcframework.zip",
-                      checksum: "997f0f066d87bb8a1357441e8d1e388abd8b93d8ce59b771702be720db1181ec")
+                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.13.0/Rokt_Widget.xcframework.zip",
+                      checksum: "656d084432c29d04af13379d8d2e8ad4ada9855dd5614ce7a5f593d16fd5936d")
     ]
 )
