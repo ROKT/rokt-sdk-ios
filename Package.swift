dfb166aa822582093c2f0b82ce454005ca2d2499
emmanuel-rokt <emmanuel.tugado@rokt.com>
2023-06-05 08:36:32 +1000

    package version update with tags

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Rokt_Widget",
-                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.14.1/Rokt_Widget.xcframework.zip",
+                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.14.2/Rokt_Widget.xcframework.zip",
                       checksum: "588816aae96940126a93c3dc50c7c84b51f79e49724424870145692b6fde2bb6")
     ]
 )
