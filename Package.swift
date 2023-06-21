50c2c09f58bc477383b83a6317e5a1023ac8e733
emmanuel-rokt <118139797+emmanuel-rokt@users.noreply.github.com>
2023-06-21 14:45:00 +1000

    bump version to 3.15.0 (#26)

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Rokt_Widget",
-                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.14.2/Rokt_Widget.xcframework.zip",
-                      checksum: "588816aae96940126a93c3dc50c7c84b51f79e49724424870145692b6fde2bb6")
+                      url: "https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/3.15.0/Rokt_Widget.xcframework.zip",
+                      checksum: "593707be79ec829b2210f304e08c582109cbb9cc4a8225e7c47c2a8d53d975e9")
     ]
 )
