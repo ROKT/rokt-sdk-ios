2d15a01e46c0b0fffa33445230b17d9ca55139aa
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-10-06 15:48:59 +1100

    Add back tag

--- .buildkite/bin/create_tag_and_update_branch.sh
+++ .buildkite/bin/create_tag_and_update_branch.sh
@@ -9,9 +9,10 @@ git config user.name "nativeappsdev"
 
 git tag -d "$1" || true
 git checkout "${BUILDKITE_BRANCH}"
+git pull
 git add Package.swift
 git add README.md
 git commit -m "$1"
 git tag -a "$1" -m "Automated release v$1"
-# git push origin "$1"
+git push origin "$1"
 git push
