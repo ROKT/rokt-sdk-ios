e8801e4774b15d4c98c76b252c043027a2bdef98
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-10-06 15:21:47 +1100

    Add automated pipeline

---
+++ .buildkite/bin/create_tag_and_update_branch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+# $1 version
+
+git config user.email "nativeappsdev@rokt.com"
+git config user.name "nativeappsdev"
+
+git tag -d "$1" || true
+git add Package.swift
+git add README.md
+git commit -m "$1"
+git tag -a "$1" -m "Automated release v$1"
+git push origin "$1"
+git push
