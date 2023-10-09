cd9946ac33e4d4c112ec05cae8b04a5e835d19d6
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-10-09 13:42:47 +1100

    Allow for tag only push

--- .buildkite/bin/create_tag_and_update_branch.sh
+++ .buildkite/bin/create_tag_and_update_branch.sh
@@ -3,6 +3,7 @@
 set -eu
 
 # $1 version
+# $2 Push to branch option (TRUE|TAG_ONLY)
 
 git config user.email "nativeappsdev@rokt.com"
 git config user.name "nativeappsdev"
@@ -15,4 +16,6 @@ git add README.md
 git commit -m "$1"
 git tag -a "$1" -m "Automated release v$1"
 git push origin "$1"
-git push -f
+if [ "$2" != "TAG_ONLY" ]; then
+    git push -f
+fi
