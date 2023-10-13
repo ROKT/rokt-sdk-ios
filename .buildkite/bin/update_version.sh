4fb1b04b3d31f92558015d9ae84c372adfb595d2
Lewis Krishnamurti <122851605+lewis-rokt@users.noreply.github.com>
2023-10-13 15:43:43 +1100

    [MP-1780] Automate SPM (#34)
    * Add automated pipeline
    
    * Test without tag
    
    * wget
    
    * Fix pipeline
    
    * Checkout branch
    
    * 3.15.5
    
    * Add back tag
    
    * Revert package
    
    * Package newline
    
    * Revert README
    
    * Force push
    
    * Allow for tag only push
    
    ---------
    
    Co-authored-by: nativeappsdev <nativeappsdev@rokt.com>

---
+++ .buildkite/bin/update_version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+# $1 iOS SDK Version
+# $2 Package.swift file location
+# $3 README.md file location
+
+FILE_LOCATION="https://rokt-eng-us-west-2-mobile-sdk-artefacts.s3.amazonaws.com/ios/"$1"/Rokt_Widget.xcframework.zip"
+brew install wget
+wget "${FILE_LOCATION}"
+CHECKSUM=$(swift package compute-checksum Rokt_Widget.xcframework.zip)
+
+perl -pi -e "s/(?<=amazonaws.com\/ios\/)(.*)(?=\/Rokt)/$1/g" $2
+perl -pi -e "s/(?<=checksum: \")(.*)(?=\")/$CHECKSUM/g" $2
+perl -pi -e "s/(?<=Select \*Up to Next Major\* with \*)(.*)(?=\*)/$1/g" $3
+perl -pi -e "s/(?<=upToNextMajor\(from: \")(.*)(?=\")/$1/g" $3
+
+rm -f Rokt_Widget.xcframework.zip
