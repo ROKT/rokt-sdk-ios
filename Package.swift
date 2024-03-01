18a202f37e76eba0920deccd185c7cae34b061be
mirannakamura <miran.nakamura@rokt.com>
2024-03-01 15:57:50 +1100

    update to 4.1.0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Rokt_Widget",
-                      url: "https://apps.rokt.com/msdk/ios/3.17.0/Rokt_Widget.xcframework.zip",
-                      checksum: "d1f245a1e5e9672cba267bd65b4c31244e14a130bcaaa8ee44b6239fb8a6fbb6"),
+                      url: "https://apps.rokt.com/msdk/ios/4.1.0/Rokt_Widget.xcframework.zip",
+                      checksum: "3eab30986bd748aebdf2edb99ff9236bc97247cbc15c865ed74725197770c9a8"),
         .target(name: "RoktWidget",
                 dependencies: [
                     .target(name: "Rokt_Widget")
