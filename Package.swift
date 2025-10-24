6193ca4f0ba03ebe918c5f462203bbb5a0f72d63
denischilik <225006499+denischilik@users.noreply.github.com>
2025-10-24 18:28:08 +0000

    Create 4.14.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Rokt_Widget",
-            url: "https://github.com/ROKT/rokt-sdk-ios/releases/download/4.14.1/Rokt_Widget.xcframework.zip",
-            checksum: "00a21e9e4dd78b778bb9d88f95dbcc5ab0c0f480dd3c60e177d9c76468f79726")
+            url: "https://github.com/ROKT/rokt-sdk-ios/releases/download/4.14.2/Rokt_Widget.xcframework.zip",
+            checksum: "b383635da51cf8e1ea23293418ca4882c324e6bf56b329e59ebb76dc367ed835")
     ]
 )
