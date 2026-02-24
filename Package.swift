3f37df09e02dfdfe85d7692c374a3356fa72a9a9
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-02-24 15:18:05 -0500

    fix: minimum ios version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Rokt-Widget",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "Rokt-Widget",
