297641e2d4be0befb4fde8a2737eb4f2ccd59c1a
Thomson Thomas <thomson.thomas@rokt.com>
2026-02-24 15:46:22 -0500

    Remove unused compiler check

--- Example/rokt/RoktApplication.swift
+++ Example/rokt/RoktApplication.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class RoktApplication: UIApplication {
 
-#if compiler(>=6)
     override func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:],
                        completionHandler completion: (@MainActor (Bool) -> Void)? = nil) {
         if !options.isEmpty {
@@ -16,22 +15,6 @@ class RoktApplication: UIApplication {
         }
         super.open(url, options: options, completionHandler: completion)
     }
-#else
-    override func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:],
-                       completionHandler completion: ((Bool) -> Void)? = nil) {
-        if !options.isEmpty {
-            print("Open URL with options: \(options)")
-            if let isRokt = options[.isRokt] as? Bool,
-               isRokt {
-                print("Open URL from Rokt is enabled")
-                super.open(url, options: [:], completionHandler: completion)
-                return
-            }
-        }
-        super.open(url, options: options, completionHandler: completion)
-
-    }
-#endif
 }
 
 public extension UIApplication.OpenExternalURLOptionsKey {
